719bab7d20912c0049717483ba3da4517dd21f3e
njager <nicholasejager@gmail.com>
2021-01-28 16:10:37 -0500

    added in parts of script from other 2d game

--- G390_Jager_OneButton/Assets/Scripts/PlayerMovement.cs
+++ G390_Jager_OneButton/Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //member variables
+    Rigidbody2D rB2D;
+    private int count;
+
+    //public variables
+    public float runSpeed;
+    public float jumpSpeed;
+    
+    public TextMeshProUGUI countText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,57 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Jump"))
+        {
+            int levelMask = LayerMask.GetMask("Level");
+
+            if (Physics2D.BoxCast(transform.position, new Vector2(1f, .1f), 0f, Vector2.down, .01f, levelMask))
+            {
+                Jump();
+            }
+        }
         
+        if (rB2D.transform.position.y <= -15f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    //adds y velocity
+    void Jump()
+    {
+        rB2D.velocity = new Vector2(rB2D.velocity.x, jumpSpeed);
+    }
+
+    //called when player collides with something
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //if the collider is the enemy, reset
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+    }
+
+    //called when player enters a trigger
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //if the trigger is a pick up, update count and play sound
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            audioSource.PlayOneShot(munchSFX);
+
+            SetCountText();
+        }
+        //if the trigger is the enemy's head, kill enemy and jump
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            audioSource.PlayOneShot(cawSFX);
+            Jump();
+        }
     }
 }
