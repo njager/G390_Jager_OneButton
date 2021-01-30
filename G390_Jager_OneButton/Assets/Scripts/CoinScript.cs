bf4ceeb65167eadc4f994cab512c6657ae06c610
njager <nicholasejager@gmail.com>
2021-01-30 11:55:00 -0500

    created coin and script

---
+++ G390_Jager_OneButton/Assets/Scripts/CoinScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    // Update is called once per frame ; rotates coin
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime);
+    }
+
+}
