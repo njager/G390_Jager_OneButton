00f4fe90e32cb0d26c28712c30bdd911e935318a
njager <nicholasejager@gmail.com>
2021-01-31 18:37:05 -0500

    commented code

--- G390_Jager_OneButton/Assets/Scripts/LevelGenerator.cs
+++ G390_Jager_OneButton/Assets/Scripts/LevelGenerator.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    //private variables
     private const float PLAYER_DISTANCE_SPAWN_LEVEL_PART = 50f;
+    private Vector3 lastEndPosition;
 
+    //level variables
     [SerializeField] private Transform levelPart_Start;
     [SerializeField] private List<Transform> levelPartList;
     [SerializeField] private Rigidbody2D player;
 
-    private Vector3 lastEndPosition;
-
+    //runs at the start of the application
         private void Awake()
     {
+        //finds the end position of the first part of the level and sets it to last end position
         lastEndPosition = levelPart_Start.Find("EndPosition").position;
 
+        //begins the level with a set number of parts
         int startingSpawnLevelParts = 5;
         for (int i = 0; i < startingSpawnLevelParts; i++)
         {
@@ -23,20 +27,24 @@ public class LevelGenerator : MonoBehaviour
         }
     }
 
+    //runs every frame
     private void Update()
     {
+        //if the player gets close to the end, spawn more parts
        if (Vector3.Distance(player.transform.position, lastEndPosition) < PLAYER_DISTANCE_SPAWN_LEVEL_PART)
         {
             //Spawn another level part
             SpawnLevelPart();
         }
     }
+    //spawns parts at the end position of the last part
     private void SpawnLevelPart()
     {
         Transform chosenLevelPart = levelPartList[Random.Range(0, levelPartList.Count)];
         Transform lastLevelPartTransform = SpawnLevelPart(chosenLevelPart, lastEndPosition);
         lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
     }
+    //sets spawned part's transform
     private Transform SpawnLevelPart(Transform levelPart, Vector3 spawnPosition)
     {
         Transform levelPartTransform = Instantiate(levelPart, spawnPosition, Quaternion.identity);
