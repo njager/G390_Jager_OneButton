239f1faf42075b88c170ea68cecf3a1f408aa0ff
njager <nicholasejager@gmail.com>
2021-01-31 18:09:59 -0500

    fixed bug on level clipping

--- G390_Jager_OneButton/Assets/Scripts/LevelGenerator.cs
+++ G390_Jager_OneButton/Assets/Scripts/LevelGenerator.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    private const float PLAYER_DISTANCE_SPAWN_LEVEL_PART = 75f;
+    private const float PLAYER_DISTANCE_SPAWN_LEVEL_PART = 50f;
 
     [SerializeField] private Transform levelPart_Start;
-    [SerializeField] private Transform levelPart_1;
+    [SerializeField] private List<Transform> levelPartList;
     [SerializeField] private Rigidbody2D player;
 
     private Vector3 lastEndPosition;
@@ -25,7 +25,7 @@ public class LevelGenerator : MonoBehaviour
 
     private void Update()
     {
-       if (Vector3.Distance(player.position, lastEndPosition) < PLAYER_DISTANCE_SPAWN_LEVEL_PART)
+       if (Vector3.Distance(player.transform.position, lastEndPosition) < PLAYER_DISTANCE_SPAWN_LEVEL_PART)
         {
             //Spawn another level part
             SpawnLevelPart();
@@ -33,12 +33,13 @@ public class LevelGenerator : MonoBehaviour
     }
     private void SpawnLevelPart()
     {
-        Transform lastLevelPartTransform = SpawnLevelPart(lastEndPosition);
+        Transform chosenLevelPart = levelPartList[Random.Range(0, levelPartList.Count)];
+        Transform lastLevelPartTransform = SpawnLevelPart(chosenLevelPart, lastEndPosition);
         lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
     }
-    private Transform SpawnLevelPart(Vector3 spawnPosition)
+    private Transform SpawnLevelPart(Transform levelPart, Vector3 spawnPosition)
     {
-        Transform levelPartTransform = Instantiate(levelPart_1, spawnPosition, Quaternion.identity);
+        Transform levelPartTransform = Instantiate(levelPart, spawnPosition, Quaternion.identity);
         return levelPartTransform;
     }
 }
