c4fe6fce3ccfc436d11fc491e63b9cc954380dcf
njager <nicholasejager@gmail.com>
2021-01-31 17:31:55 -0500

    gotfirst part of infintie level spawn working

--- G390_Jager_OneButton/Assets/Scripts/LevelGenerator.cs
+++ G390_Jager_OneButton/Assets/Scripts/LevelGenerator.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    private const float PLAYER_DISTANCE_SPAWN_LEVEL_PART = 75f;
+
     [SerializeField] private Transform levelPart_Start;
     [SerializeField] private Transform levelPart_1;
+    [SerializeField] private Rigidbody2D player;
+
+    private Vector3 lastEndPosition;
 
         private void Awake()
     {
-        
-        SpawnLevelPart(levelPart_Start.Find("EndPosition").position);
-        SpawnLevelPart(new Vector3(19, 3) + new Vector3(10, 0));
-        SpawnLevelPart(new Vector3(19, 3) + new Vector3(10 + 10, 0));
+        lastEndPosition = levelPart_Start.Find("EndPosition").position;
+
+        int startingSpawnLevelParts = 5;
+        for (int i = 0; i < startingSpawnLevelParts; i++)
+        {
+            SpawnLevelPart();
+        }
     }
 
-    private void SpawnLevelPart(Vector3 spawnPosition)
+    private void Update()
     {
-        Instantiate(levelPart_1, spawnPosition, Quaternion.identity);
+       if (Vector3.Distance(player.position, lastEndPosition) < PLAYER_DISTANCE_SPAWN_LEVEL_PART)
+        {
+            //Spawn another level part
+            SpawnLevelPart();
+        }
+    }
+    private void SpawnLevelPart()
+    {
+        Transform lastLevelPartTransform = SpawnLevelPart(lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
+    }
+    private Transform SpawnLevelPart(Vector3 spawnPosition)
+    {
+        Transform levelPartTransform = Instantiate(levelPart_1, spawnPosition, Quaternion.identity);
+        return levelPartTransform;
     }
 }
